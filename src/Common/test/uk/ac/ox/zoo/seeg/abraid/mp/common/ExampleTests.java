a794b8e16e3b5f91b6f09b2f65a945715e85db4f
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-01-20 15:24:21 +0000

    Example test to verify build processes.

--- src/Common/test/uk/ac/ox/zoo/seeg/abraid/mp/common/ExampleTests.java
+++ src/Common/test/uk/ac/ox/zoo/seeg/abraid/mp/common/ExampleTests.java
@@ -1,7 +1,12 @@
 package uk.ac.ox.zoo.seeg.abraid.mp.common;
 
-/**
- * Created by zool1112 on 20/01/14.
- */
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.fest.assertions.api.Assertions.*;
+
 public class ExampleTests {
+    @Test
+    public void testMultiply() {
+        assertThat(1).isEqualTo(1).isPositive().isNotZero().isLessThan(2);
+    }
 }
