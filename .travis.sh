31f60f69a361dc15f50ed634e8a15cffd2f98fa0
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-02-28 11:30:38 +0000

    Add requested comment to build script

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-echo -en 'travis_fold:start:Log\r'
+echo -en 'travis_fold:start:Log\r' # Causes travis to create a folding group in the stdout output with the title "Log"
 echo "================= Full Log ================="
 annotate-output +"" ant full 2>&1 | tee build.log
 EXIT=${PIPESTATUS[0]}
-echo -en 'travis_fold:end:Log\r'
+echo -en 'travis_fold:end:Log\r' # End of "Log" fold group
 if [ $EXIT -ne 0 ]
 then
     echo "================== Summary =================="
     echo "Build exited with code: $EXIT"
-    grep -E "(^\ E\:)|(\ O\:\ \[echo\]\ \[Summary\])" build.log
+    grep -E "(^\ E\:)|(^\ O\:\ \[echo\]\ \[Summary\])" build.log
 fi
 exit $EXIT
