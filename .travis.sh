96290f1e903bc482e288039c31d8cdde78614346
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-02-28 09:23:03 +0000

    Try travis output folding. Also make build fail to test.

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+echo -en 'travis_fold:start:Log\\r'
 echo "================= Full Log ================="
 annotate-output +"" ant full 2>&1 | tee build.log
 EXIT=${PIPESTATUS[0]}
-echo ""
-echo "================== Error Summary =================="
-cat build.log | grep "^\ E\:"
+echo -en 'travis_fold:end:Log\\r'
+if cat build.log | grep "^\ E\:" ; then
+    echo "================== Error Summary =================="
+    cat build.log | grep "^\ E\:"
+fi
 exit $EXIT
