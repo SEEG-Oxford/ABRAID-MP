3aad1ff73cd3d046af5450f9f5eb4e7d0a48e84a
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-02-28 09:50:12 +0000

    Missing bits from last commit

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-echo -en 'travis_fold:start:Log\\r'
+echo -en 'travis_fold:start:Log\r'
 echo "================= Full Log ================="
 annotate-output +"" ant full 2>&1 | tee build.log
 EXIT=${PIPESTATUS[0]}
-echo -en 'travis_fold:end:Log\\r'
+echo -en 'travis_fold:end:Log\r'
 if [ $EXIT -ne 0 ]
 then
     echo "================== Summary =================="
     echo "Build exited with code: $EXIT"
-    grep "(^\ E\:)|(\ O\:\ \[echo\]\ Summary)" build.log
+    grep "(^\ E\:)|(\ O\:\ \[echo\]\ \[Summary\])" build.log
 fi
 exit $EXIT
