8424d3f286098a67db38eec4c673334198d85a4e
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-03-03 09:59:48 +0000

    + Make findbugs output to log instead of file. + Improve travis build summaries

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,56 @@
 #!/usr/bin/env bash
-echo -en 'travis_fold:start:Log\r' # Causes travis to create a folding group in the stdout output with the title "Log"
+
+# Cause travis to create a folding group in the stdout output with the title "Log"
+echo -en 'travis_fold:start:Log\r'
+
 echo "================= Full Log ================="
-annotate-output +"" ant full 2>&1 | tee build.log
-EXIT=${PIPESTATUS[0]}
-echo -en 'travis_fold:end:Log\r' # End of "Log" fold group
-if [ $EXIT -ne 0 ]
+# Check if "annotate-ouput" is available
+if command -v annotate-ouput >/dev/null 2>&1
 then
-    echo "================== Summary =================="
-    echo "Build exited with code: $EXIT"
-    grep -E "(^\ E\:)|(^\ O\:\ \[echo\]\ \[Summary\])" build.log
+    # Run "ant full" with an annotated log file
+    annotate-output +"" ant full -k 2>&1 | tee build.log
+else
+    # Fallback. Run "ant full" with log file. (i.e. not on travis).
+    # Note: stderr lines will not be aggregated into the summary.
+    ant full -k 2>&1 | tee build.log
 fi
+# Get the exit code of "ant full"
+EXIT=${PIPESTATUS[0]}
+
+# End of "Log" fold group
+echo -en 'travis_fold:end:Log\r'
+
+# Define a set of paterns to match in the build log for inclusion the summary output
+# 1. Lines marked as error output, 2. Lines marked as summary ouput, 3. Findbugs output, 4. Checkstyle output, 5. JUnit warnings
+INCLUDE_PATERNS="\
+(^\ E\:)|\
+\[Summary\]|\
+\[findbugs\]|\
+\[checkstyle\]|\
+\[junit\](.)*ERROR|\
+\[junit\](.)*SKIPPED|\
+\[junit\](.)*FAILED|\
+Cannot\ execute"
+
+# Define a set of paterns to match in the build log for excludsion from the summary output
+# 1. Unnessary findbugs outputs (we only want the warnings), 2. Unnessary checkstyle outputs (we only want the warnings)
+EXCLUDE_PATERNS="\
+\[findbugs\]\ Executing\ findbugs|\
+\[findbugs\]\ Running\ FindBugs|\
+\[findbugs\]\ Exit\ code\ set\ to|\
+\[findbugs\]\ Calculating\ exit|\
+\[findbugs\]\ Setting\ |\
+\[findbugs\]\ Java\ Result|\
+\[findbugs\]\ Warnings\ generated|\
+\[findbugs\].*Findbugs2\.execute|\
+\[findbugs\].*Findbugs\.runMain|\
+\[findbugs\].*Findbugs2\.main|\
+\[findbugs\]\ java.io.IOExection: No files|\
+\[checkstyle\]\ Running"
+
+# Copy a subset of the full log to the screen as a summary
+echo "================== Summary =================="
+grep -E "${INCLUDE_PATERNS}" build.log | grep -v -E "${EXCLUDE_PATERNS}"
+
+# Return exit code of "ant full"
 exit $EXIT
