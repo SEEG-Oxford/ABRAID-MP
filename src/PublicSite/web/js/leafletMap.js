075b50b649ae2b39abe375602d1b8339518aa20a
Helena Patching <helena.patching@tessella.com>
2014-03-06 16:36:42 +0000

    Highlight and select point with mouse events

--- src/PublicSite/web/js/leafletMap.js
+++ src/PublicSite/web/js/leafletMap.js
@@ -24,15 +24,50 @@ var baseLayer = L.tileLayer.wms(geoServerUrl, {
 // Add disease occurrence points to map
 // First add empty geoJson layer to map, with styling options, then get JSON data using an AJAX request
 var locationLayer = L.geoJson([], {
-    pointToLayer: locationLayerPoint
+    pointToLayer: locationLayerPoint,
+    style: locationLayerStyle
 }).addTo(map);
 
+// Define a circle, instead of the default leaflet marker, with listeners for mouse events
 function locationLayerPoint(feature, latlng) {
-    return L.circleMarker(latlng, {
-        stroke: false,
-        fill: true,
-        fillOpacity: 0.8,
-        radius: 4
+    return L.circleMarker(latlng).on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: selectFeature
+    });
+}
+
+// Define default style for unselected points
+function locationLayerStyle() { return {
+    stroke: false,
+    fill: true,
+    color:'#9e1e71',
+    fillOpacity: 0.8,
+    radius: 4
+};
+}
+
+// Reset to default style when a point is unselected (by clicking anywhere else on the map)
+map.on('click', function() { locationLayer.setStyle(locationLayerStyle); });
+
+// Change a point's colour on roll-over
+function highlightFeature() {
+    $("#source").innerHTML = feature.properties.name;
+    this.setStyle({ color:'#84a872' });
+}
+
+// Return to default colour
+function resetHighlight() {
+    this.setStyle({ color:'#9e1e71'});
+}
+
+// Reset previous point to default style, so only one point can be selected at a time, and make selected point larger
+function selectFeature() {
+    locationLayer.setStyle(locationLayerStyle);
+    this.setStyle({
+        color:'#84a872',
+        stroke: true,
+        radius: 7
     });
 }
 
