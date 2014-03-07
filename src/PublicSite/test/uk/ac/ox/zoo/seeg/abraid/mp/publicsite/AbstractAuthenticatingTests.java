ed824d4b8df69d5758102044e4ec62b67e452787
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-03-07 11:38:26 +0000

    Tests for current user service

---
+++ src/PublicSite/test/uk/ac/ox/zoo/seeg/abraid/mp/publicsite/AbstractAuthenticatingTests.java
@@ -0,0 +1,34 @@
+package uk.ac.ox.zoo.seeg.abraid.mp.publicsite;
+
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.domain.PublicSiteUser;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by zool1112 on 07/03/14.
+ */
+public class AbstractAuthenticatingTests {
+    @Before
+    public void setupSecurityContext() {
+        SecurityContextHolder.setContext(new SecurityContextImpl());
+        setupAnonymousUser();
+    }
+
+    protected static void setupCurrentUser(PublicSiteUser user) {
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(user);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    protected static void setupAnonymousUser() {
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn("anonymousUser"); // This is the documented behavior of spring
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
---
+++ src/PublicSite/test/uk/ac/ox/zoo/seeg/abraid/mp/publicsite/security/CurrentUserServiceTest.java
@@ -0,0 +1,43 @@
+package uk.ac.ox.zoo.seeg.abraid.mp.publicsite.security;
+
+import org.junit.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.AbstractAuthenticatingTests;
+import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.domain.PublicSiteUser;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Tests the CurrentUserServiceImpl class.
+ * Copyright (c) 2014 University of Oxford
+ */
+public class CurrentUserServiceTest extends AbstractAuthenticatingTests {
+    @Test
+    public void getCurrentUserReturnsCorrectAuthorizedUser() throws Exception {
+        // Arrange
+        PublicSiteUser expectation = mock(PublicSiteUser.class);
+        setupCurrentUser(expectation);
+        CurrentUserService target = new CurrentUserServiceImpl();
+
+        // Act
+        PublicSiteUser result = target.getCurrentUser();
+
+        // Assert
+        assertThat(result).isSameAs(expectation);
+    }
+
+    @Test
+    public void getCurrentUserReturnsNullForAnonymousUser() throws Exception {
+        // Arrange
+        CurrentUserService target = new CurrentUserServiceImpl();
+
+        // Act
+        PublicSiteUser result = target.getCurrentUser();
+
+        // Assert
+        assertThat(result).isNull();
+    }
+
+}
