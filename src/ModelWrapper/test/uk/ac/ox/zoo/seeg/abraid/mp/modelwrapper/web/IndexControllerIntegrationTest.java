d5c81714750286e53bd7fd3e1fb4bed6300c94da
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-03-11 14:39:24 +0000

    Integration tests for index controller

---
+++ src/ModelWrapper/test/uk/ac/ox/zoo/seeg/abraid/mp/modelwrapper/web/IndexControllerIntegrationTest.java
@@ -0,0 +1,58 @@
+package uk.ac.ox.zoo.seeg.abraid.mp.modelwrapper.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import static org.hamcrest.text.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Integration test for the root ModelWrapper controller.
+ * Copyright (c) 2014 University of Oxford
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:ModelWrapper/web/WEB-INF/abraid-servlet-beans.xml")
+@WebAppConfiguration("file:ModelWrapper/web")
+public class IndexControllerIntegrationTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private IndexController controller;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() {
+        // Process mock annotations
+        MockitoAnnotations.initMocks(this);
+
+        // Setup Spring test in standalone mode
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void indexPageReturnsCorrectContent() throws Exception {
+        this.mockMvc
+                .perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<title>ABRAID-MP: ModelWrapper</title>")));
+    }
+
+
+
+}
