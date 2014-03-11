d5905e88039d36704a5451280f785bb8e1dd579d
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-03-11 15:40:43 +0000

    Make integration tests use spring context more sensibly.

--- src/PublicSite/test/uk/ac/ox/zoo/seeg/abraid/mp/publicsite/web/DataValidationControllerIntegrationTest.java
+++ src/PublicSite/test/uk/ac/ox/zoo/seeg/abraid/mp/publicsite/web/DataValidationControllerIntegrationTest.java
@@ -2,23 +2,25 @@ package uk.ac.ox.zoo.seeg.abraid.mp.publicsite.web;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.DiseaseOccurrence;
 import uk.ac.ox.zoo.seeg.abraid.mp.common.service.ExpertService;
+import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.AbstractAuthenticatingTests;
 import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.domain.PublicSiteUser;
 import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.json.AbstractDiseaseOccurrenceGeoJsonTests;
-import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.json.geojson.GeoJsonObjectMapper;
-import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.security.CurrentUserService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -28,39 +30,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Integration tests for the data validation controller.
  * Copyright (c) 2014 University of Oxford
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+        "file:PublicSite/web/WEB-INF/abraid-servlet-beans.xml",
+        "classpath:uk/ac/ox/zoo/seeg/abraid/mp/publicsite/web/mockServices.xml"})
+@WebAppConfiguration("file:PublicSite/web")
 public class DataValidationControllerIntegrationTest {
-    @Mock
-    private ExpertService expertServiceMock;
+    @Autowired
+    private ExpertService expertService;
 
-    @Mock
-    private CurrentUserService currentUserServiceMock;
-
-    @Mock
-    private PublicSiteUser loggedInUser;
-
-    @InjectMocks
+    @Autowired
     private DataValidationController controller;
 
     private MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
     @Before
     public void setup() {
-        // Process mock annotations
-        MockitoAnnotations.initMocks(this);
-
-        // Setup jackson
-        MappingJackson2HttpMessageConverter jackson = new MappingJackson2HttpMessageConverter();
-        jackson.setObjectMapper(new GeoJsonObjectMapper());
-
         // Setup Spring test in standalone mode
         this.mockMvc = MockMvcBuilders
-                .standaloneSetup(controller)
-                .setMessageConverters(jackson)
+                .webAppContextSetup(webApplicationContext)
                 .build();
 
         // Setup user
-        when(currentUserServiceMock.getCurrentUser()).thenReturn(loggedInUser);
+        PublicSiteUser loggedInUser = mock(PublicSiteUser.class);
         when(loggedInUser.getId()).thenReturn(1);
+        AbstractAuthenticatingTests.setupCurrentUser(loggedInUser);
     }
 
     @Test
@@ -69,7 +66,7 @@ public class DataValidationControllerIntegrationTest {
         occurrences.add(AbstractDiseaseOccurrenceGeoJsonTests.defaultDiseaseOccurrence());
         occurrences.add(AbstractDiseaseOccurrenceGeoJsonTests.defaultDiseaseOccurrence());
 
-        when(expertServiceMock.getDiseaseOccurrencesYetToBeReviewed(anyInt(), anyInt())).thenReturn(occurrences);
+        when(expertService.getDiseaseOccurrencesYetToBeReviewed(anyInt(), anyInt())).thenReturn(occurrences);
 
         this.mockMvc.perform(
                 get(DataValidationController.GEOWIKI_BASE_URL + "/diseases/1/occurrences"))
@@ -80,7 +77,7 @@ public class DataValidationControllerIntegrationTest {
 
     @Test
     public void occurrenceResourceRejectsInvalidNumericId() throws Exception {
-        when(expertServiceMock.getDiseaseOccurrencesYetToBeReviewed(anyInt(), anyInt()))
+        when(expertService.getDiseaseOccurrencesYetToBeReviewed(anyInt(), anyInt()))
                 .thenThrow(new IllegalArgumentException());
 
         this.mockMvc.perform(
