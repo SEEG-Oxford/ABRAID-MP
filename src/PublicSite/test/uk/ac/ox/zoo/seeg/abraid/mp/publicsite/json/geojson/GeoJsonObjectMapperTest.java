46dfeb1fbc1bc8ac6dc16895079564135fa17fa4
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-03-12 15:26:05 +0000

    Resolve issue of empty feature collections causing invalid geojson

--- src/PublicSite/test/uk/ac/ox/zoo/seeg/abraid/mp/publicsite/json/geojson/GeoJsonObjectMapperTest.java
+++ src/PublicSite/test/uk/ac/ox/zoo/seeg/abraid/mp/publicsite/json/geojson/GeoJsonObjectMapperTest.java
@@ -3,9 +3,14 @@ package uk.ac.ox.zoo.seeg.abraid.mp.publicsite.json.geojson;
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.Test;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.DiseaseOccurrence;
+import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.json.AbstractDiseaseOccurrenceGeoJsonTests;
+import uk.ac.ox.zoo.seeg.abraid.mp.publicsite.json.GeoJsonDiseaseOccurrenceFeatureCollection;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TimeZone;
 
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -29,5 +34,34 @@ public class GeoJsonObjectMapperTest {
 
         // Assert
         assertThat(result).isEqualTo("\"" + ISODateTimeFormat.dateTime().print(jodaTime) + "\"");
+    }
+
+    @Test
+    public void constructorForGeoJsonObjectMapperConfiguresEmptyArraySerialization() throws Exception {
+        // Arrange
+        GeoJsonFeatureCollection empty = new GeoJsonDiseaseOccurrenceFeatureCollection(new ArrayList<DiseaseOccurrence>());
+        OutputStream stream = new ByteArrayOutputStream();
+
+        // Act
+        GeoJsonObjectMapper target = new GeoJsonObjectMapper();
+        target.writeValue(stream, empty);
+        String result = stream.toString();
+
+        // Assert
+        assertThat(result).isEqualTo("{\"type\":\"FeatureCollection\",\"crs\":{\"type\":\"name\",\"properties\":{\"name\":\"urn:ogc:def:crs:EPSG::4326\"}},\"features\":[]}");
+    }
+
+    @Test
+    public void constructorForGeoJsonObjectMapperConfiguresSingletonArraySerialization() throws Exception {
+        // Arrange
+        GeoJsonFeatureCollection singleton = new GeoJsonDiseaseOccurrenceFeatureCollection(Arrays.asList(AbstractDiseaseOccurrenceGeoJsonTests.defaultDiseaseOccurrence()));
+        OutputStream stream = new ByteArrayOutputStream();
+        // Act
+        GeoJsonObjectMapper target = new GeoJsonObjectMapper();
+        target.writeValue(stream, singleton);
+        String result = stream.toString();
+
+        // Assert
+        assertThat(result).isEqualTo("{\"type\":\"FeatureCollection\",\"crs\":{\"type\":\"name\",\"properties\":{\"name\":\"urn:ogc:def:crs:EPSG::4326\"}},\"features\":[{\"type\":\"Feature\",\"id\":1,\"geometry\":{\"type\":\"Point\",\"coordinates\":[1.0,-1.0]},\"properties\":{\"locationName\":\"locationName\",\"countryName\":\"countryName\",\"alert\":{\"title\":\"title\",\"summary\":\"summary\",\"url\":\"url\",\"feedName\":\"feedName\",\"publicationDate\":\"1970-01-01T00:00:00.000Z\"},\"diseaseOccurrenceStartDate\":\"1970-01-01T00:00:00.000Z\"}}]}");
     }
 }
