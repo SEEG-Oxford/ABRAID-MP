1c37da76ce779d3268b94e7c18d68da26debc735
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-04-01 11:04:27 +0100

    Add some missing tests

--- src/ModelWrapper/test/uk/ac/ox/zoo/seeg/abraid/mp/modelwrapper/configuration/RunConfigurationFactoryTest.java
+++ src/ModelWrapper/test/uk/ac/ox/zoo/seeg/abraid/mp/modelwrapper/configuration/RunConfigurationFactoryTest.java
@@ -2,15 +2,30 @@ package uk.ac.ox.zoo.seeg.abraid.mp.modelwrapper.configuration;
 
 import org.junit.Test;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * Tests for RunConfigurationFactoryImpl.
  * Copyright (c) 2014 University of Oxford
  */
 public class RunConfigurationFactoryTest {
     @Test
-    public void createDefaultConfiguration() throws Exception {
+    public void createDefaultConfigurationUsesCorrectDefaults() throws Exception {
         // Arrange
+        ConfigurationService configurationService = mock(ConfigurationService.class);
+        when(configurationService.getCacheDirectory()).thenReturn("expectation1");
+        when(configurationService.getModelRepositoryVersion()).thenReturn("expectation2");
+        RunConfigurationFactory target = new RunConfigurationFactoryImpl(configurationService);
+
         // Act
+        RunConfiguration result = target.createDefaultConfiguration();
+
         // Assert
+        assertThat(result.getRunName()).isEqualTo("run");
+        assertThat(result.getBaseDir().getName()).isEqualTo("expectation1");
+        assertThat(result.getMaxRuntime()).isEqualTo(Integer.MAX_VALUE);
+        assertThat(result.getModelVersion()).isEqualTo("expectation2");
     }
 }
--- src/ModelWrapper/test/uk/ac/ox/zoo/seeg/abraid/mp/modelwrapper/model/InputDataManagerTest.java
+++ src/ModelWrapper/test/uk/ac/ox/zoo/seeg/abraid/mp/modelwrapper/model/InputDataManagerTest.java
@@ -1,22 +1,78 @@
 package uk.ac.ox.zoo.seeg.abraid.mp.modelwrapper.model;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.DiseaseOccurrence;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.web.json.GeoJsonDiseaseOccurrenceFeatureCollection;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.web.json.geojson.GeoJsonNamedCrs;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static uk.ac.ox.zoo.seeg.abraid.mp.testutils.AbstractDiseaseOccurrenceGeoJsonTests.defaultDiseaseOccurrence;
 
 /**
  * Tests for InputDataManagerImpl.
  * Copyright (c) 2014 University of Oxford
  */
 public class InputDataManagerTest {
-    @Test
-    public void writeData() throws Exception {
-        // Arrange
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder(); // SUPPRESS CHECKSTYLE VisibilityModifier
 
+    @Test
+    public void writeDataCreatesCorrectCsv() throws Exception {
+        // Arrange
+        DiseaseOccurrence occurrence = defaultDiseaseOccurrence();
+        GeoJsonDiseaseOccurrenceFeatureCollection data = new GeoJsonDiseaseOccurrenceFeatureCollection(
+                Arrays.asList(occurrence));
+        InputDataManager target = new InputDataManagerImpl();
+        File dir = testFolder.newFolder();
 
         // Act
-
+        target.writeData(data, dir);
+        String result = FileUtils.readFileToString(Paths.get(dir.toString(), "outbreak.csv").toFile());
 
         // Assert
+        assertThat(result).isEqualTo("-1.0,1.0,PRECISE,0.5" + System.lineSeparator());
+    }
 
+    @Test
+    public void writeDataRequiresEPSG4326() throws Exception {
+        // Arrange
+        DiseaseOccurrence occurrence = defaultDiseaseOccurrence();
+        GeoJsonDiseaseOccurrenceFeatureCollection data = new GeoJsonDiseaseOccurrenceFeatureCollection(
+                Arrays.asList(occurrence));
+        InputDataManager target = new InputDataManagerImpl();
+        File dir = testFolder.newFolder();
 
+        // Act
+        data.setCrs(new GeoJsonNamedCrs());
+        catchException(target).writeData(data, dir);
+
+        // Assert
+        assertThat(caughtException()).isInstanceOf(IllegalArgumentException.class).hasMessage("Only EPSG4326 is supported.");
+    }
+
+    @Test
+    public void writeDataRejectsFeatureLevelCrs() throws Exception {
+        // Arrange
+        DiseaseOccurrence occurrence = defaultDiseaseOccurrence();
+        GeoJsonDiseaseOccurrenceFeatureCollection data = new GeoJsonDiseaseOccurrenceFeatureCollection(
+                Arrays.asList(occurrence));
+        InputDataManager target = new InputDataManagerImpl();
+        File dir = testFolder.newFolder();
+
+        // Act
+        data.getFeatures().get(0).setCrs(new GeoJsonNamedCrs());
+        catchException(target).writeData(data, dir);
+
+        // Assert
+        assertThat(caughtException()).isInstanceOf(IllegalArgumentException.class).hasMessage("Feature level CRS are not supported.");
     }
 }
