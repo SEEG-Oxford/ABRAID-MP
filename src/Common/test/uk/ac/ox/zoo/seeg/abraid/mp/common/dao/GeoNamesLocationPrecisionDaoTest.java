718bd34fd6b3e0cb92408fe0f748678877ba46f7
Ed Wiles <edward.wiles@tessella.com>
2014-04-02 11:58:14 +0100

    Update disease groups, HealthMap diseases, GeoNames location precisions, feeds. Results from a meeting with Catherine and import of production data.

--- src/Common/test/uk/ac/ox/zoo/seeg/abraid/mp/common/dao/GeoNamesLocationPrecisionDaoTest.java
+++ src/Common/test/uk/ac/ox/zoo/seeg/abraid/mp/common/dao/GeoNamesLocationPrecisionDaoTest.java
@@ -21,6 +21,6 @@ public class GeoNamesLocationPrecisionDaoTest extends AbstractSpringIntegrationT
     @Test
     public void getAllGeoNamesLocationPrecisions() {
         List<GeoNamesLocationPrecision> list = geoNamesLocationPrecisionDao.getAll();
-        assertThat(list).hasSize(197);
+        assertThat(list).hasSize(214);
     }
 }
