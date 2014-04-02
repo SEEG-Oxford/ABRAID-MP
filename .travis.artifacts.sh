53d2507b764aefd21944f02098b773b9498260f6
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-04-02 14:58:07 +0100

    Travis artifact archiving test.

---
+++ .travis.artifacts.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#   A project build script for use with travis-ci.org
+#   Copyright (c) 2014 University of Oxford
+
+
+if $TRAVIS_SECURE_ENV_VARS; then
+    #if git describe --tags --exact-match HEAD &> /dev/null;
+    if true
+    then
+        # Only archive artifacts if we are on a tag
+
+        # Setup key
+        echo -n $id_rsa_{00..30} >> ~/.ssh/id_rsa_base64
+        base64 --decode --ignore-garbage ~/.ssh/id_rsa_base64 > ~/.ssh/id_rsa
+        chmod 600 ~/.ssh/id_rsa
+
+        # Get verison
+        VERSION=`git describe`
+
+        # Send data
+        scp -oStrictHostKeyChecking=no -r artifacts abraid-travis@map1.zoo.ox.ac.uk:~/artifacts/$VERSION
+
+        # Cleanup key
+        rm ~/.ssh/id_rsa_base64
+        rm ~/.ssh/id_rsa
+    fi
+    # TBD "deloy" tags to test server?
+    # TBD "deloy" master to test server? (check TRAVIS_PULL_REQUEST as well as TRAVIS_BRANCH)
+fi
+
