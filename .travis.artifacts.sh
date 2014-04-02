b7a602435b36a86a798e6fe3b63da2dffe2a2040
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-04-02 15:34:54 +0100

    debug info

--- .travis.artifacts.sh
+++ .travis.artifacts.sh
@@ -4,9 +4,11 @@
 
 
 if $TRAVIS_SECURE_ENV_VARS; then
+    echo "1"
     #if git describe --tags --exact-match HEAD &> /dev/null;
     if true
     then
+        echo "2"
         # Only archive artifacts if we are on a tag
 
         # Setup key
@@ -14,12 +16,18 @@ if $TRAVIS_SECURE_ENV_VARS; then
         base64 --decode --ignore-garbage ~/.ssh/id_rsa_base64 > ~/.ssh/id_rsa
         chmod 600 ~/.ssh/id_rsa
 
+        sha256sum ~/.ssh/id_rsa_base64
+        sha256sum ~/.ssh/id_rsa
+
+        echo "3"
         # Get verison
         VERSION=`git describe`
 
+        echo $VERSION
         # Send data
         scp -oStrictHostKeyChecking=no -r artifacts abraid-travis@map1.zoo.ox.ac.uk:~/artifacts/$VERSION
 
+        echo "4"
         # Cleanup key
         rm ~/.ssh/id_rsa_base64
         rm ~/.ssh/id_rsa
