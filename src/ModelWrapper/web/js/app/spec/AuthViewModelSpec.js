18849440f5c9183953d68350058f37643ccb6dae
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-04-09 15:24:01 +0100

    Move custom rule to it's own file (reuse).

--- src/ModelWrapper/web/js/app/spec/AuthViewModelSpec.js
+++ src/ModelWrapper/web/js/app/spec/AuthViewModelSpec.js
@@ -2,50 +2,14 @@
  * Copyright (c) 2014 University of Oxford
  */
 /*global define:false, describe:false, it:false, expect:false, beforeEach:false, jasmine:false*/
-define([ 'app/AuthViewModel', 'underscore', 'ko' ], function(AuthViewModel, _, ko) {
+define([ 'app/AuthViewModel', 'ko', 'app/spec/ruleMatcher' ], function(AuthViewModel, ko, ruleMatcher) {
     "use strict";
-
-    var customMatchers = {
-        toHaveValidationRule: function(util, customEqualityTesters) {
-            return {
-                compare: function(actual, expected) {
-
-                    if (expected === undefined) {
-                        expected = {};
-                    }
-                    if (expected.name === undefined) {
-                        expected.name = '';
-                    }
-                    if (expected.params === undefined) {
-                        expected.name = null;
-                    }
-
-                    var result = {};
-
-                    var matchingRules = _(actual.rules()).where({ rule: expected.name });
-                    if (matchingRules.length === 1) {
-                        if(util.equals(matchingRules[0].params, expected.params, customEqualityTesters)) {
-                            result.pass = true;
-                        } else {
-                            result.pass = false;
-                            result.message = "Expected rule '" + expected.name + "' was present but the parameter was '" + matchingRules[0].params + "' instead of the expected '" + expected.params + "'.";
-                        }
-                    } else {
-                        result.pass = false;
-                        result.message = "Expected rule '" + expected.name + "' be present but it was not in: [" + _(actual.rules()).pluck('rule') + "].";
-                    }
-
-                    return result;
-                }
-            };
-        }
-    };
 
     describe("The auth view model", function() {
         var vm = {};
         var initVM = function(makeValidatable) {
             return function() {
-                jasmine.addMatchers(customMatchers);
+                jasmine.addMatchers({ toHaveValidationRule: ruleMatcher });
 
                 if (!makeValidatable) {
                     vm = new AuthViewModel("foo");
@@ -63,7 +27,6 @@ define([ 'app/AuthViewModel', 'underscore', 'ko' ], function(AuthViewModel, _, k
             });
 
             it("is required", function() {
-                jasmine.addMatchers(customMatchers);
                 expect(vm.username).toHaveValidationRule({name:'required', params:true});
             });
 
