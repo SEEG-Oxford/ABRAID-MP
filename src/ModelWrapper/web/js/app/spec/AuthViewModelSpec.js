3439ee8c9f9592e176b91bd33be35bbadbff07d6
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-04-09 15:18:01 +0100

    Refactor example tests to use a custom matcher

--- src/ModelWrapper/web/js/app/spec/AuthViewModelSpec.js
+++ src/ModelWrapper/web/js/app/spec/AuthViewModelSpec.js
@@ -1,14 +1,52 @@
 /* A suite of tests for the AuthViewModel AMD.
  * Copyright (c) 2014 University of Oxford
  */
-/*global define:false, describe:false, it:false, expect:false, beforeEach:false*/
+/*global define:false, describe:false, it:false, expect:false, beforeEach:false, jasmine:false*/
 define([ 'app/AuthViewModel', 'underscore', 'ko' ], function(AuthViewModel, _, ko) {
     "use strict";
+
+    var customMatchers = {
+        toHaveValidationRule: function(util, customEqualityTesters) {
+            return {
+                compare: function(actual, expected) {
+
+                    if (expected === undefined) {
+                        expected = {};
+                    }
+                    if (expected.name === undefined) {
+                        expected.name = '';
+                    }
+                    if (expected.params === undefined) {
+                        expected.name = null;
+                    }
+
+                    var result = {};
+
+                    var matchingRules = _(actual.rules()).where({ rule: expected.name });
+                    if (matchingRules.length === 1) {
+                        if(util.equals(matchingRules[0].params, expected.params, customEqualityTesters)) {
+                            result.pass = true;
+                        } else {
+                            result.pass = false;
+                            result.message = "Expected rule '" + expected.name + "' was present but the parameter was '" + matchingRules[0].params + "' instead of the expected '" + expected.params + "'.";
+                        }
+                    } else {
+                        result.pass = false;
+                        result.message = "Expected rule '" + expected.name + "' be present but it was not in: [" + _(actual.rules()).pluck('rule') + "].";
+                    }
+
+                    return result;
+                }
+            };
+        }
+    };
 
     describe("The auth view model", function() {
         var vm = {};
         var initVM = function(makeValidatable) {
             return function() {
+                jasmine.addMatchers(customMatchers);
+
                 if (!makeValidatable) {
                     vm = new AuthViewModel("foo");
                 } else {
@@ -25,11 +63,12 @@ define([ 'app/AuthViewModel', 'underscore', 'ko' ], function(AuthViewModel, _, k
             });
 
             it("is required", function() {
-                expect(_(vm.username.rules()).pluck('rule')).toContain('required');
+                jasmine.addMatchers(customMatchers);
+                expect(vm.username).toHaveValidationRule({name:'required', params:true});
             });
 
             it("has the minimum username complexity enforced", function() {
-                expect(_(vm.username.rules()).pluck('rule')).toContain('usernameComplexity');
+                expect(vm.username).toHaveValidationRule({name:'usernameComplexity', params:true});
             });
         });
 
@@ -41,11 +80,11 @@ define([ 'app/AuthViewModel', 'underscore', 'ko' ], function(AuthViewModel, _, k
             });
 
             it("is required", function() {
-                expect(_(vm.password.rules()).pluck('rule')).toContain('required');
+                expect(vm.password).toHaveValidationRule({name:'required', params:true});
             });
 
             it("has the minimum password complexity enforced", function() {
-                expect(_(vm.password.rules()).pluck('rule')).toContain('passwordComplexity');
+                expect(vm.password).toHaveValidationRule({name:'passwordComplexity', params:true});
             });
         });
 
@@ -57,16 +96,15 @@ define([ 'app/AuthViewModel', 'underscore', 'ko' ], function(AuthViewModel, _, k
             });
 
             it("is required", function() {
-                expect(_(vm.passwordConfirmation.rules()).pluck('rule')).toContain('required');
+                expect(vm.passwordConfirmation).toHaveValidationRule({name:'required', params:true});
             });
 
             it("has the minimum password complexity enforced", function() {
-                expect(_(vm.passwordConfirmation.rules()).pluck('rule')).toContain('passwordComplexity');
+                expect(vm.passwordConfirmation).toHaveValidationRule({name:'passwordComplexity', params:true});
             });
 
             it("must match the other password", function() {
-                expect(_(vm.passwordConfirmation.rules()).pluck('rule')).toContain('areSame');
-                expect(_(vm.passwordConfirmation.rules()).findWhere({ rule: "areSame" }).params).toBe(vm.password);
+                expect(vm.passwordConfirmation).toHaveValidationRule({name:'areSame', params:vm.password});
             });
         });
 
