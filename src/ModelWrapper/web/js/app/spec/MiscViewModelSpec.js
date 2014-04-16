7b4af3fa70547e67b0b6c66766dbe5001eacba19
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-04-16 15:36:09 +0100

    Typo

--- src/ModelWrapper/web/js/app/spec/MiscViewModelSpec.js
+++ src/ModelWrapper/web/js/app/spec/MiscViewModelSpec.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2014 University of Oxford
  */
 /*global define:false, describe:false, it:false, expect:false, beforeEach:false, afterEach:false, jasmine:false*/
-define([ 'app/MiscViewModel', 'ko', 'underscore', 'app/spec/util/ruleMatcher', 'app/spec/util/observableMatcher', 'app/spec/util/Squire' ], function(MiscViewModel, ko, _, ruleMatcher, observableMatcher, Squire) {
+define([ 'app/MiscViewModel', 'ko', 'underscore', 'app/spec/util/ruleMatcher', 'app/spec/util/observableMatcher', 'app/spec/util/squire' ], function(MiscViewModel, ko, _, ruleMatcher, observableMatcher, Squire) {
     "use strict";
 
     describe("The misc view model", function() {
