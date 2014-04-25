bc69887a8a4e3c779fd058d875da3349c8d4eada
Ed Wiles <edward.wiles@tessella.com>
2014-04-25 14:20:18 +0100

    Fix index names and duplicated lines

--- db/migrate_shapefiles.sql
+++ db/migrate_shapefiles.sql
@@ -17,12 +17,16 @@ SET simplified_geom = aust.geom
 FROM admin_unit_simplified_tropical aust
 WHERE aut.gaul_code = aust.gaul_code;
 
-ALTER TABLE admin_unit_global ALTER simplified_geom SET NOT NULL;
-ALTER TABLE admin_unit_tropical ALTER simplified_geom SET NOT NULL;
-
 -- Add spatial indexes to the newly-populated columns
-CREATE INDEX admin_unit_global_simplified_geom_gist ON admin_unit_global USING GIST (simplified_geom);
-CREATE INDEX admin_unit_tropical_simplified_geom_gist ON admin_unit_tropical USING GIST (simplified_geom);
+CREATE INDEX ix_admin_unit_global_simplified_geom ON admin_unit_global USING GIST (simplified_geom);
+CREATE INDEX ix_admin_unit_tropical_simplified_geom ON admin_unit_tropical USING GIST (simplified_geom);
+
+-- Rename other spatial indexes to conform to our naming standard
+ALTER INDEX admin_unit_global_geom_gist RENAME TO ix_admin_unit_global_geom;
+ALTER INDEX admin_unit_qc_geom_gist RENAME TO ix_admin_unit_qc_geom;
+ALTER INDEX admin_unit_tropical_geom_gist RENAME TO ix_admin_unit_tropical_geom;
+ALTER INDEX country_geom_gist RENAME TO ix_country_geom;
+ALTER INDEX land_sea_border_geom_gist RENAME TO ix_land_sea_border_geom;
 
 -- Add NOT NULL constraints to all geometry columns (they are nullable in tables.sql because of the test data)
 ALTER TABLE admin_unit_global ALTER simplified_geom SET NOT NULL;
