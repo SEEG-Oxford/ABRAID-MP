dc71f30911b0be08a932acd16f8261c6f881574a
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-05-28 16:55:28 +0100

    Move model setup out of the http request thread

---
+++ src/ModelWrapper/src/uk/ac/ox/zoo/seeg/abraid/mp/modelwrapper/model/ModelRunnerAsyncWrapperImpl.java
@@ -0,0 +1,49 @@
+package uk.ac.ox.zoo.seeg.abraid.mp.modelwrapper.model;
+
+import uk.ac.ox.zoo.seeg.abraid.mp.common.web.json.GeoJsonDiseaseOccurrenceFeatureCollection;
+import uk.ac.ox.zoo.seeg.abraid.mp.modelwrapper.config.run.RunConfiguration;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Provides a means for triggering model runs where the workspace setup is done asynchronously.
+ * Copyright (c) 2014 University of Oxford
+ */
+public class ModelRunnerAsyncWrapperImpl implements ModelRunnerAsyncWrapper {
+    private final ExecutorService pool = Executors.newFixedThreadPool(1);
+    private final ModelRunner modelRunner;
+
+    public ModelRunnerAsyncWrapperImpl(ModelRunner modelRunner) {
+        this.modelRunner = modelRunner;
+    }
+
+    /**
+     * Asynchronously starts a new model run with the given configuration.
+     * @param configuration The model run configuration.
+     * @param occurrenceData The occurrence data to model with.
+     * @param extentWeightings The mapping from GAUL code to disease extent class weighting.
+     * @return The process handler for the launched process.
+     */
+    @Override
+    public Future<ModelProcessHandler> startModel(final RunConfiguration configuration,
+                                                  final GeoJsonDiseaseOccurrenceFeatureCollection occurrenceData,
+                                                  final Map<Integer, Integer> extentWeightings) {
+        return pool.submit(new Callable<ModelProcessHandler>() {
+            @Override
+            public ModelProcessHandler call() throws Exception {
+                ModelProcessHandler handler = null;
+                try {
+                    handler = modelRunner.runModel(configuration, occurrenceData, extentWeightings);
+                } catch (Exception e) {
+                    // Need to consider failures (ie failures in setup for R rather than in R)
+                }
+                return handler;
+            }
+        });
+    }
+
+}
