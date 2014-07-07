4e6669e813cc1161ceb703dc8a2432a36b74fbed
Helena Patching <helena.patching@tessella.com>
2014-07-07 15:33:58 +0100

    Username change

--- config/deploy/up_db.sh
+++ config/deploy/up_db.sh
@@ -11,7 +11,7 @@ echo "database.name=$DB_NAME" >> database.properties
 ant create.database
 
 # Create an application user
-psql -wq -U "$PG_ABRAID_USER" -d "$DB_NAME" --command "INSERT INTO expert (name, email, hashed_password, is_administrator) VALUES ( 'Mr Test', '$ABRAID_USER_EMAIL', '$ABRAID_USER_PASS', true )"
+psql -wq -U "$PG_ABRAID_USER" -d "$DB_NAME" --command "INSERT INTO expert (name, email, hashed_password, is_administrator) VALUES ( 'Dr Test', '$ABRAID_USER_EMAIL', '$ABRAID_USER_PASS', true )"
 
 # Load historic healthmap data
 cd $BASE/external/healthmap
