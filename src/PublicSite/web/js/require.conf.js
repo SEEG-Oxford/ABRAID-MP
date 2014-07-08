6e2ef2ce143078e7850fad806692cf7359df0e4e
Helena Patching <helena.patching@tessella.com>
2014-07-08 15:25:00 +0100

    Add KO validation to PS

--- src/PublicSite/web/js/require.conf.js
+++ src/PublicSite/web/js/require.conf.js
@@ -14,6 +14,10 @@
             "bootstrap": "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.3/js/bootstrap",
             "bootstrap.extensions": "https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.2/js/jasny-bootstrap",
             "knockout": "https://cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-debug",
+            // Move to cdn path when next version is published (error template refreshing bug)
+            "knockout.validation": baseUrl + "js/lib/knockout-validation.min",
+            // Move to cdn path when next version is published (AMD support)
+            "knockout.bootstrap": baseUrl + "js/lib/knockout-bootstrap.min",
             "knockout-postbox": baseUrl + "js/lib/knockout-postbox",
             "underscore": "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore",
             "domReady": "https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady",
