f3a12d708d8bcaa130edf770ef83ea9c04f8b6cd
Laurence Hudson <laurence.hudson@tessella.com>
2014-07-23 10:20:49 +0100

    Update MapViewSpec.js

--- src/PublicSite/web/js/app/spec/datavalidation/MapViewSpec.js
+++ src/PublicSite/web/js/app/spec/datavalidation/MapViewSpec.js
@@ -1,7 +1,7 @@
 /* A suite of tests for the MapView AMD.
  * Copyright (c) 2014 University of Oxford
  */
-/* jshint unused:false */ // Ignore unused "MapView", this is an placeholder until these tests are written.
+/* jshint unused:false */ // Ignore unused "MapView", this is a placeholder until these tests are written.
 define(["app/datavalidation/MapView"], function (MapView) {
     "use strict";
 
