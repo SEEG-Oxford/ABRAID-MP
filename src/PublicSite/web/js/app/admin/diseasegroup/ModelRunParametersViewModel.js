7954f7ee3c3ede09fe5b24404a8981aed8b4604c
Helena Patching <helena.patching@tessella.com>
2014-07-24 18:34:05 +0100

    Writeable computed takes null value when occursInAfrica is true

--- src/PublicSite/web/js/app/admin/diseasegroup/ModelRunParametersViewModel.js
+++ src/PublicSite/web/js/app/admin/diseasegroup/ModelRunParametersViewModel.js
@@ -12,9 +12,21 @@ define([
         self.minNewOccurrences = ko.observable().extend({ digit: true, min: 0 });
         self.minDataVolume = ko.observable().extend({ digit: true, min: 0 });
         self.minDistinctCountries = ko.observable().extend({ digit: true, min: 0 });
-        self.minHighFrequencyCountries = ko.observable().extend({ digit: true, min: 0 });
-        self.highFrequencyThreshold = ko.observable().extend({ digit: true, min: 0 });
+
         self.occursInAfrica = ko.observable();
+        self.minHighFrequencyCountriesValue = ko.observable();
+        self.minHighFrequencyCountries = ko.computed({
+            read: function () { return self.occursInAfrica() ? self.minHighFrequencyCountriesValue() : null; },
+            write: function (value) { self.minHighFrequencyCountriesValue(value); },
+            owner: self
+        }).extend({ digit: true, min: 0 });
+
+        self.highFrequencyThresholdValue = ko.observable();
+        self.highFrequencyThreshold = ko.computed({
+            read: function () { return self.occursInAfrica() ? self.highFrequencyThresholdValue() : null; },
+            write: function (value) { self.highFrequencyThresholdValue(value); },
+            owner: this
+        }).extend({ digit: true, min: 0 });
 
         ko.postbox.subscribe(diseaseGroupSelectedEventName, function (diseaseGroup) {
             self.minNewOccurrences((diseaseGroup.minNewOccurrences || "").toString());
