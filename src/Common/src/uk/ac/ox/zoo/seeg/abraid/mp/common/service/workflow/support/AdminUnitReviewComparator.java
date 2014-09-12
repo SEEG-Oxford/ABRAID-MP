e8a289a4f0c73f5de368c9dec3f622f500289bc4
Helena Patching <helena.patching@tessella.com>
2014-09-12 11:02:34 +0100

    Swap chained comparator for CompareToBuilder

--- src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/service/workflow/support/AdminUnitReviewComparator.java
+++ src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/service/workflow/support/AdminUnitReviewComparator.java
@@ -1,54 +1,22 @@
 package uk.ac.ox.zoo.seeg.abraid.mp.common.service.workflow.support;
 
+import org.apache.commons.lang.builder.CompareToBuilder;
 import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.AdminUnitReview;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 /**
- * Custom comparator to sort list of reviews by experts (asc), admin unit gaul code (asc), then date (desc).
+ * Custom comparator to sort list of reviews by expert (asc), admin unit gaul code (asc), then date (desc).
  * Copyright (c) 2014 University of Oxford
  */
 public class AdminUnitReviewComparator implements Comparator<AdminUnitReview> {
 
-    private Comparator<AdminUnitReview> expertComparator;
-    private Comparator<AdminUnitReview> gaulCodeComparator;
-    private Comparator<AdminUnitReview> dateComparator;
-
-    public AdminUnitReviewComparator() {
-         expertComparator = new Comparator<AdminUnitReview>() {
-            @Override
-            public int compare(AdminUnitReview o1, AdminUnitReview o2) {
-                return o1.getExpert().getId().compareTo(o2.getExpert().getId());
-            }
-        };
-        gaulCodeComparator = new Comparator<AdminUnitReview>() {
-            @Override
-            public int compare(AdminUnitReview o1, AdminUnitReview o2) {
-                return o1.getAdminUnitGlobalOrTropicalGaulCode().compareTo(o2.getAdminUnitGlobalOrTropicalGaulCode());
-            }
-        };
-        dateComparator = new Comparator<AdminUnitReview>() {
-            @Override
-            public int compare(AdminUnitReview o1, AdminUnitReview o2) {
-                return o1.getCreatedDate().compareTo(o2.getCreatedDate());
-            }
-        };
-    }
-
     @Override
     public int compare(AdminUnitReview o1, AdminUnitReview o2) {
-        List<Comparator<AdminUnitReview>> listComparators =
-                Arrays.asList(expertComparator, gaulCodeComparator, Collections.reverseOrder(dateComparator));
-
-        for (Comparator<AdminUnitReview> comparator : listComparators) {
-            int result = comparator.compare(o1, o2);
-            if (result != 0) {
-                return result;
-            }
-        }
-        return 0;
+        return new CompareToBuilder()
+                .append(o1.getExpert().getId(), o2.getExpert().getId()) // ascending
+                .append(o1.getAdminUnitGlobalOrTropicalGaulCode(), o2.getAdminUnitGlobalOrTropicalGaulCode()) // ascend.
+                .append(o2.getCreatedDate(), o1.getCreatedDate()) // descending
+                .toComparison();
     }
 }
