56e1fb7e40c85d6de4029146f6a74c567f488bc4
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-09-24 15:49:18 +0100

    DTO for effect curve data

---
+++ src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/dto/csv/AbstractCsvCovariateInfluence.java
@@ -0,0 +1,61 @@
+package uk.ac.ox.zoo.seeg.abraid.mp.common.dto.csv;
+
+/**
+ * CSV DTO which  represents the comon fields associated with the influence of a covariate file on a model run.
+ * Copyright (c) 2014 University of Oxford
+ */
+public abstract class AbstractCsvCovariateInfluence {
+    private String covariateName;
+    private String covariateDisplayName;
+    private Double meanInfluence;
+    private Double upperQuantile;
+    private Double lowerQuantile;
+
+    public AbstractCsvCovariateInfluence() {
+    }
+
+    public String getCovariateName() {
+        return covariateName;
+    }
+
+    //@JsonProperty("")
+    public void setCovariateName(String covariateName) {
+        this.covariateName = covariateName;
+    }
+
+    public String getCovariateDisplayName() {
+        return covariateDisplayName;
+    }
+
+    //@JsonProperty("display")
+    public void setCovariateDisplayName(String covariateDisplayName) {
+        this.covariateDisplayName = covariateDisplayName;
+    }
+
+    public Double getMeanInfluence() {
+        return meanInfluence;
+    }
+
+    //@JsonProperty("mean")
+    public void setMeanInfluence(Double meanInfluence) {
+        this.meanInfluence = meanInfluence;
+    }
+
+    public Double getUpperQuantile() {
+        return upperQuantile;
+    }
+
+    //@JsonProperty("97.5")
+    public void setUpperQuantile(Double upperQuantile) {
+        this.upperQuantile = upperQuantile;
+    }
+
+    public Double getLowerQuantile() {
+        return lowerQuantile;
+    }
+
+    //@JsonProperty("2.5%")
+    public void setLowerQuantile(Double lowerQuantile) {
+        this.lowerQuantile = lowerQuantile;
+    }
+}
--- src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/dto/csv/CsvEffectCurveCovariateInfluence.java
+++ src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/dto/csv/CsvEffectCurveCovariateInfluence.java
@@ -7,25 +7,38 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * CSV DTO which represents the influence of a covariate file on a model run.
+ * CSV DTO which represents the influence of a covariate file on a model run through its full range of values,
+ * for plotting in an effect curve.
  * Copyright (c) 2014 University of Oxford
  */
-public class CsvCovariateInfluence extends AbstractCsvCovariateInfluence {
+public class CsvEffectCurveCovariateInfluence extends AbstractCsvCovariateInfluence {
+    private Double covariateValue;
+
     /**
      * Parses a collection of CsvCovariateInfluence entries from a csv string (header row expected).
      * @param csv The csv string.
      * @return A collection of CsvCovariateInfluence entries.
-     * @throws IOException Thrown if the parsing fails.
+     * @throws java.io.IOException Thrown if the parsing fails.
      */
-    public static List<CsvCovariateInfluence> readFromCSV(String csv) throws IOException {
+    public static List<CsvEffectCurveCovariateInfluence> readFromCSV(String csv) throws IOException {
         CsvSchema schema = CsvSchema.builder()
                 .setSkipFirstDataRow(true)
+                .addColumn("")
                 .addColumn("covariateName")
+                .addColumn("covariateValue")
                 .addColumn("meanInfluence", CsvSchema.ColumnType.NUMBER)
                 .addColumn("lowerQuantile", CsvSchema.ColumnType.NUMBER)
                 .addColumn("upperQuantile", CsvSchema.ColumnType.NUMBER)
                 .build();
 
-        return ParseUtils.readFromCsv(csv, CsvCovariateInfluence.class, schema);
+        return ParseUtils.readFromCsv(csv, CsvEffectCurveCovariateInfluence.class, schema);
+    }
+
+    public Double getCovariateValue() {
+        return covariateValue;
+    }
+
+    public void setCovariateValue(Double covariateValue) {
+        this.covariateValue = covariateValue;
     }
 }
