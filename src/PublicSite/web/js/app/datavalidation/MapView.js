24964459a7a7fe0c52924df7e2a10777c4aa38cf
Helena Patching <helena.patching@tessella.com>
2014-10-15 17:29:49 +0100

    Make "legend" consistent with "latest occurrences panel"

--- src/PublicSite/web/js/app/datavalidation/MapView.js
+++ src/PublicSite/web/js/app/datavalidation/MapView.js
@@ -378,7 +378,7 @@ define([
         var legend = L.control({position: "bottomleft"});
         legend.onAdd = function () {
             var div = L.DomUtil.create("div", "legend");
-            div.innerHTML = "<span style='text-decoration:underline'>Current classification</span><br>" +
+            div.innerHTML = "<span style='text-align: center'><strong>Current classification</strong></span><br><hr>" +
                 _((_(extentClassColour).pairs()).map(function (pair) {
                     return createLegendRow(pair[0], pair[1][1]);
                 })).join("");
