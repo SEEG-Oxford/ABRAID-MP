d1d6335c47abd800194cbf02f6ac5103c6486a40
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-11-11 12:13:08 +0000

    Doc fixes

--- src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/dto/json/views/DiseaseSynchronisationJsonView.java
+++ src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/dto/json/views/DiseaseSynchronisationJsonView.java
@@ -3,8 +3,8 @@ package uk.ac.ox.zoo.seeg.abraid.mp.common.dto.json.views;
 import uk.ac.ox.zoo.seeg.abraid.mp.common.dto.json.views.support.BaseView;
 
 /**
- * A placeholder class for use with Jackson's JsonView annotations.
- * Defines th
+ * A placeholder interface for use with Jackson's JsonView annotations (as a Class object).
+ * Defines the "disease synchronisation" view.
  * Copyright (c) 2014 University of Oxford
  */
 public interface DiseaseSynchronisationJsonView extends BaseView { }
