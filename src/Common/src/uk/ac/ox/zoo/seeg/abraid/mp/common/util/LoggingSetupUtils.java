5e39209c53e1ebba87b7ac8ae312a0eb9e1e1c30
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-11-20 11:09:03 +0000

    PR fixes

--- src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/util/LoggingSetupUtils.java
+++ src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/util/LoggingSetupUtils.java
@@ -1,5 +1,6 @@
 package uk.ac.ox.zoo.seeg.abraid.mp.common.util;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.Priority;
@@ -10,7 +11,7 @@ import uk.ac.ox.zoo.seeg.abraid.mp.common.config.SmtpConfiguration;
  * Logging configuration utilities.
  * Copyright (c) 2014 University of Oxford
  */
-public class LoggingSetupUtil {
+public class LoggingSetupUtils {
     private static final PatternLayout LAYOUT = new PatternLayout("%d{ISO8601} %5p [%t] %c - %m%n");
     private final String loggingContextName;
     private final boolean useEmailLogging;
@@ -18,8 +19,8 @@ public class LoggingSetupUtil {
     private final String emailToAddress;
     private final SmtpConfiguration emailSmtpConfig;
 
-    public LoggingSetupUtil(String loggingContextName, boolean useEmailLogging,
-                            String emailFromAddress, String emailToAddress, SmtpConfiguration emailSmtpConfig) {
+    public LoggingSetupUtils(String loggingContextName, boolean useEmailLogging,
+                             String emailFromAddress, String emailToAddress, SmtpConfiguration emailSmtpConfig) {
         this.loggingContextName = loggingContextName;
         this.useEmailLogging = useEmailLogging;
         this.emailFromAddress = emailFromAddress;
@@ -43,7 +44,7 @@ public class LoggingSetupUtil {
         emailAppender.setTo(emailToAddress);
         emailAppender.setLayout(LAYOUT);
         emailAppender.setBufferSize(10);
-        emailAppender.setThreshold(Priority.ERROR);
+        emailAppender.setThreshold(Level.ERROR);
         emailAppender.setSubject(String.format("%s Error Alert", loggingContextName));
         emailAppender.activateOptions();
         Logger.getRootLogger().addAppender(emailAppender);
