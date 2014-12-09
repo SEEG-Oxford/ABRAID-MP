2e5e6d4fb71a800006e7487a1e9dd41be614f1e4
Helena Patching <helena.patching@tessella.com>
2014-12-09 15:41:42 +0000

    Display 'feed name ' if 'alert title' is null

--- src/Common/test/uk/ac/ox/zoo/seeg/abraid/mp/common/dto/json/GeoJsonAlertTest.java
+++ src/Common/test/uk/ac/ox/zoo/seeg/abraid/mp/common/dto/json/GeoJsonAlertTest.java
@@ -1,41 +1,69 @@
 package uk.ac.ox.zoo.seeg.abraid.mp.common.dto.json;
 
+import org.junit.Before;
 import org.junit.Test;
 import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.Alert;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.Feed;
 import uk.ac.ox.zoo.seeg.abraid.mp.testutils.AbstractDiseaseOccurrenceGeoJsonTests;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 /**
  * Tests for GeoJsonAlert.
  * Copyright (c) 2014 University of Oxford
  */
 public class GeoJsonAlertTest extends AbstractDiseaseOccurrenceGeoJsonTests {
+    private String summary = "foo2";
+    private String feedName = "foo3";
+    private String feedLanguage = "foo4";
+    private String url = "foo5";
+    private Alert alert;
+
+    @Before
+    public void setUp() {
+        alert = newAlert(summary, feedName, feedLanguage, url);
+    }
+
+    private Alert newAlert(String summary, String feedName, String feedLanguage, String url) {
+        Alert alert = new Alert();
+        alert.setSummary(summary);
+        alert.setFeed(new Feed(feedName, feedLanguage));
+        alert.setUrl(url);
+        return alert;
+    }
+
     @Test
     public void constructorForGeoJsonAlertBindsParametersCorrectly() throws Exception {
         // Arrange
-        String expectedTitle = "foo1";
-        String expectedSummary = "foo2";
-        String expectedFeedName = "foo3";
-        String expectedFeedLanguage = "foo4";
-        String expectedUrl = "foo5";
-
-        Alert alert = defaultAlert();
-        when(alert.getTitle()).thenReturn(expectedTitle);
-        when(alert.getSummary()).thenReturn(expectedSummary);
-        when(alert.getFeed().getName()).thenReturn(expectedFeedName);
-        when(alert.getFeed().getLanguage()).thenReturn(expectedFeedLanguage);
-        when(alert.getUrl()).thenReturn(expectedUrl);
+        String title = "foo1";
+        alert.setTitle(title);
 
         // Act
         GeoJsonAlert result = new GeoJsonAlert(alert);
 
         // Assert
-        assertThat(result.getFeedName()).isEqualTo(expectedFeedName);
-        assertThat(result.getFeedLanguage()).isEqualTo(expectedFeedLanguage);
-        assertThat(result.getUrl()).isEqualTo(expectedUrl);
-        assertThat(result.getSummary()).isEqualTo(expectedSummary);
-        assertThat(result.getTitle()).isEqualTo(expectedTitle);
+        assertThat(result.getTitle()).isEqualTo(title);
+        assertProperties(result);
+
+    }
+
+    @Test
+    public void constructorForGeoJsonAlertBindsParametersCorrectlyWhenAlertTitleNotDefined() throws Exception {
+        // Arrange
+        alert.setTitle(null);
+
+        // Act
+        GeoJsonAlert result = new GeoJsonAlert(alert);
+
+        // Assert
+        assertThat(result.getTitle()).isEqualTo(feedName);
+        assertProperties(result);
+    }
+
+    private void assertProperties(GeoJsonAlert result) {
+        assertThat(result.getSummary()).isEqualTo(summary);
+        assertThat(result.getFeedName()).isEqualTo(feedName);
+        assertThat(result.getFeedLanguage()).isEqualTo(feedLanguage);
+        assertThat(result.getUrl()).isEqualTo(url);
     }
 }
