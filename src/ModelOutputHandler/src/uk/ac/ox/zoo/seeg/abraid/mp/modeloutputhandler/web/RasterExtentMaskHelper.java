ce15c3cd26e12fb8b4843d75c0d7a5b25f3db205
Laurence Hudson <laurence.hudson@tessella.com>
2015-01-08 17:32:06 +0000

    Adding raster transform to mask out absence regions

---
+++ src/ModelOutputHandler/src/uk/ac/ox/zoo/seeg/abraid/mp/modeloutputhandler/web/RasterExtentMaskHelper.java
@@ -0,0 +1,55 @@
+package uk.ac.ox.zoo.seeg.abraid.mp.modeloutputhandler.web;
+
+import org.apache.log4j.Logger;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.DiseaseExtentClass;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.service.core.DiseaseService;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.util.RasterTransformation;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.util.RasterUtils;
+
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * foo
+ * Copyright (c) 2014 University of Oxford
+ */
+public class RasterExtentMaskHelper {
+    private static final Logger LOGGER = Logger.getLogger(RasterExtentMaskHelper.class);
+    private static final Object LOG_TRANSFORMING_RASTER_DATA =
+            "Masking out known absence regions in model output raster.";
+
+    private final int extentAbsenceValue;
+
+    public RasterExtentMaskHelper(DiseaseService diseaseService) {
+        this.extentAbsenceValue = diseaseService.getDiseaseExtentClass(DiseaseExtentClass.ABSENCE).getWeighting();
+    }
+
+    public void maskRaster(File targetFile, File sourceRasterFile, File extentRasterFile) throws IOException {
+        File[] referenceRasterFiles = new File[] { extentRasterFile };
+        RasterUtils.transformRaster(sourceRasterFile, targetFile, referenceRasterFiles, new RasterTransformation() {
+            @Override
+            public void transform(WritableRaster raster, Raster[] referenceRasters) {
+                transformRaster(raster, referenceRasters[0]);
+            }
+        });
+    }
+
+    private void transformRaster(WritableRaster raster, Raster extentRaster) {
+        LOGGER.info(LOG_TRANSFORMING_RASTER_DATA);
+
+        for (int i = 0; i < raster.getWidth(); i++) {
+            for (int j = 0; j < raster.getHeight(); j++) {
+                int extentValue = extentRaster.getSample(i, j, 0);
+
+                if (extentValue == extentAbsenceValue) {
+                    int value = raster.getSample(i, j, 0);
+                    if (value != RasterUtils.NO_DATA_VALUE) {
+                        raster.setSample(i, j, 0, 0);
+                    }
+                }
+            }
+        }
+    }
+}
