8963439053223af3e567c65b634f2c92b59a3b39
Laurence Hudson <laurence.hudson@tessella.com>
2015-01-09 15:30:01 +0000

    Fix raster disposal

--- src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/util/RasterUtils.java
+++ src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/util/RasterUtils.java
@@ -6,14 +6,17 @@ import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridCoverageFactory;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.coverage.grid.io.GridCoverage2DReader;
+import org.geotools.factory.Hints;
 import org.geotools.gce.geotiff.GeoTiffFormat;
 import org.geotools.gce.geotiff.GeoTiffReader;
 import org.geotools.gce.geotiff.GeoTiffWriteParams;
 import org.geotools.geometry.Envelope2D;
+import org.geotools.resources.image.ImageUtilities;
 import org.opengis.coverage.grid.GridCoverageWriter;
 import org.opengis.parameter.GeneralParameterValue;
 import org.opengis.parameter.ParameterValue;
 
+import javax.media.jai.PlanarImage;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.File;
@@ -41,16 +44,21 @@ public final class RasterUtils {
             "Applying raster transforming raster.";
     private static final String LOG_READING_RASTER =
             "Reading raster: %s.";
+    private static final String CANNOT_FIND_FILE_MESSAGE = "Cannot find raster file %s";
+
     public static final int NO_DATA_VALUE = -9999;
     public static final int UNKNOWN_VALUE = +9999;
     private static final AbstractGridFormat GEOTIFF_FORMAT = new GeoTiffFormat();
     private static final float GEOTIFF_COMPRESSION_LEVEL = 0.9F;
     private static final String GEOTIFF_COMPRESSION_TYPE = "Deflate";
+    private static final Hints RASTER_READ_HINTS =
+            new Hints(Hints.DEFAULT_COORDINATE_REFERENCE_SYSTEM, GeometryUtils.WGS_84_CRS);
 
     public static void transformRaster(File sourceRasterFile, File targetRasterFile, File[] referenceRasterFiles,
                                        RasterTransformation transformation) throws IOException {
+        int numberOfReferenceRasters = referenceRasterFiles.length;
         GridCoverage2D sourceRaster = null;
-        GridCoverage2D[] referenceRasters = new GridCoverage2D[referenceRasterFiles.length];
+        GridCoverage2D[] referenceRasters = new GridCoverage2D[numberOfReferenceRasters];
 
         try {
             // Load source raster
@@ -64,12 +72,12 @@ public final class RasterUtils {
 
             // Load reference rasters
             LOGGER.info(LOG_LOADING_REFERENCE_RASTERS);
-            for (int i=0; i < referenceRasterFiles.length; i++) {
+            for (int i=0; i < numberOfReferenceRasters; i++) {
                 referenceRasters[i] = loadRaster(referenceRasterFiles[i]);
             }
             // Extract raw data from the reference rasters
-            Raster[] referenceRastersData = new Raster[referenceRasters.length];
-            for (int i=0; i < referenceRasters.length; i++) {
+            Raster[] referenceRastersData = new Raster[numberOfReferenceRasters];
+            for (int i=0; i < numberOfReferenceRasters; i++) {
                 referenceRastersData[i] = referenceRasters[i].getRenderedImage().getData();
             }
 
@@ -81,25 +89,31 @@ public final class RasterUtils {
             LOGGER.info(String.format(LOG_SAVING_RASTER, targetRasterFile.getAbsolutePath()));
             saveRaster(targetRasterFile, rasterData, rasterExtent, rasterProperties);
         } finally {
-            disposeResource(sourceRaster);
-            for (GridCoverage2D referenceRaster : referenceRasters) {
-                disposeResource(referenceRaster);
+            disposeRaster(sourceRaster);
+            for (int i=0; i < numberOfReferenceRasters; i++) {
+                disposeRaster(referenceRasters[i]);
             }
         }
     }
 
     public static GridCoverage2D loadRaster(File location) throws IOException {
         LOGGER.info(String.format(LOG_READING_RASTER, location.getAbsolutePath()));
-        GridCoverage2DReader reader = null;
-        try {
-            reader = new GeoTiffReader(location);
-            return reader.read(null);
-        } catch (Exception e) {
-            final String message = String.format(LOG_FAILED_TO_READ_RASTER, location.toString());
-            LOGGER.error(message, e);
-            throw new IOException(message, e);
-        } finally {
-            disposeResource(reader);
+        if (location.exists()) {
+            GridCoverage2DReader reader = null;
+            try {
+                reader = new GeoTiffReader(location, RASTER_READ_HINTS);
+                return reader.read(null);
+            } catch (Exception e) {
+                final String message = String.format(LOG_FAILED_TO_READ_RASTER, location.toString());
+                LOGGER.error(message, e);
+                throw new IOException(message, e);
+            } finally {
+                disposeResource(reader);
+            }
+        } else {
+            String message = String.format(CANNOT_FIND_FILE_MESSAGE, location.getAbsolutePath());
+            LOGGER.error(message);
+            throw new IllegalArgumentException(message);
         }
     }
 
@@ -119,7 +133,7 @@ public final class RasterUtils {
             LOGGER.error(message, e);
             throw new IOException(message, e);
         } finally {
-            disposeResource(targetRaster);
+            disposeRaster(targetRaster);
             disposeResource(writer);
         }
     }
@@ -136,8 +150,9 @@ public final class RasterUtils {
         };
     }
 
-    private static void disposeResource(GridCoverage2D raster) {
+    public static void disposeRaster(GridCoverage2D raster) {
         if (raster != null) {
+            ImageUtilities.disposePlanarImageChain((PlanarImage) raster.getRenderedImage());
             raster.dispose(true);
         }
     }
