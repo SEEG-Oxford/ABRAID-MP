11997e7d8e004230e770d53aaa0365d20e4c591a
Laurence Hudson <laurence.hudson@tessella.com>
2015-02-13 11:39:03 +0000

    Document email setter

--- src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/domain/Expert.java
+++ src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/domain/Expert.java
@@ -124,6 +124,10 @@ public class Expert {
         return email;
     }
 
+    /**
+     * Sets the email address of the user. The email address is coerced to lower case.
+     * @param email The email address of the user.
+     */
     public void setEmail(String email) {
         String lowerEmail = (email == null) ? null : email.toLowerCase();
         this.email = lowerEmail;
