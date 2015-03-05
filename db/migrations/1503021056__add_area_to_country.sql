a90eee2b6544183bc1818d2087d5faeacb5a9121
Laurence Hudson <laurence.hudson@tessella.com>
2015-03-05 11:28:15 +0000

    Make sure area migration works with test data

--- db/migrations/1503021056__add_area_to_country.sql
+++ db/migrations/1503021056__add_area_to_country.sql
@@ -1,6 +1,6 @@
--- Add area to country (in square km).
+-- Add area to country (in square km). "COALESCE with 0" is for compatibility with "create.test.database", where some countries lack geoms.
 -- Copyright (c) 2014 University of Oxford
 
 ALTER TABLE country ADD COLUMN area double precision;
-UPDATE country SET area=st_area(st_transform(geom, 3410))/1000000;
+UPDATE country SET area=COALESCE(st_area(st_transform(geom, 3410))/1000000, 0);
 ALTER TABLE country ALTER COLUMN area SET NOT NULL;
