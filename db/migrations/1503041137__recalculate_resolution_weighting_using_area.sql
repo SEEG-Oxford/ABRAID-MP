bdc944dcc1679968216f07c51e622d4a4da9365a
Laurence Hudson <laurence.hudson@tessella.com>
2015-03-06 13:07:36 +0000

    Fixes to resolution weighting update migration from staging test

--- db/migrations/1503041137__recalculate_resolution_weighting_using_area.sql
+++ db/migrations/1503041137__recalculate_resolution_weighting_using_area.sql
@@ -5,5 +5,6 @@ ALTER TABLE location ALTER COLUMN resolution_weighting DROP NOT NULL;
 UPDATE location SET resolution_weighting=NULL WHERE has_passed_qc=FALSE;
 UPDATE location SET resolution_weighting=1 WHERE precision='PRECISE' AND has_passed_qc=TRUE;
 UPDATE location SET resolution_weighting=((1.0/ln(25.0/115000.0))*ln((1.0/115000.0)*(SELECT area FROM admin_unit_qc WHERE admin_unit_qc.gaul_code=location.admin_unit_qc_gaul_code))) WHERE precision IN ('ADMIN1', 'ADMIN2') AND has_passed_qc=TRUE AND admin_unit_qc_gaul_code IS NOT NULL;
-UPDATE location SET resolution_weighting=((1.0/ln(25.0/115000.0))*ln((1.0/115000.0)*(SELECT area FROM country WHERE country.gaul_code=location.country_gaul_code))) WHERE precision='COUNTRY' AND has_passed_qc=TRUE AND admin_unit_qc_gaul_code IS NOT NULL;
+UPDATE location SET resolution_weighting=((1.0/ln(25.0/115000.0))*ln((1.0/115000.0)*(SELECT area FROM country WHERE country.gaul_code=location.country_gaul_code))) WHERE precision='COUNTRY' AND has_passed_qc=TRUE AND country_gaul_code IS NOT NULL;
+UPDATE location SET resolution_weighting=0 WHERE resolution_weighting<0 AND has_passed_qc=TRUE;
 UPDATE location SET resolution_weighting=1 WHERE resolution_weighting>1 AND has_passed_qc=TRUE;
