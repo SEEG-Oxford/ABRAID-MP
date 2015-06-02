dad61fd108e409a02ed9550b944de6c35d006445
Laurence Hudson <laurence.hudson@tessella.com>
2015-06-02 11:34:33 +0100

    Ensure that "i dont know" occurrence reviews dont crash the expert score calculations

--- src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/service/workflow/support/WeightingsCalculator.java
+++ src/Common/src/uk/ac/ox/zoo/seeg/abraid/mp/common/service/workflow/support/WeightingsCalculator.java
@@ -2,17 +2,14 @@ package uk.ac.ox.zoo.seeg.abraid.mp.common.service.workflow.support;
 
 import ch.lambdaj.function.convert.Converter;
 import org.apache.log4j.Logger;
-import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.DiseaseOccurrence;
-import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.DiseaseOccurrenceReview;
-import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.DiseaseOccurrenceStatus;
-import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.Expert;
+import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.*;
 import uk.ac.ox.zoo.seeg.abraid.mp.common.service.core.DiseaseService;
 import uk.ac.ox.zoo.seeg.abraid.mp.common.service.core.ExpertService;
 
 import java.util.*;
 
 import static ch.lambdaj.Lambda.*;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
@@ -183,11 +180,13 @@ public class WeightingsCalculator {
      */
     public void updateExpertsWeightings() {
         List<DiseaseOccurrenceReview> allReviews = diseaseService.getAllDiseaseOccurrenceReviews();
-        if (allReviews.size() == 0) {
+        List<DiseaseOccurrenceReview> reviews = filter(
+                having(on(DiseaseOccurrenceReview.class).getResponse(), notNullValue()), allReviews);
+        if (reviews.size() == 0) {
             logger.info(NOT_UPDATING_WEIGHTINGS_OF_EXPERTS);
         } else {
-            logger.info(String.format(RECALCULATING_WEIGHTINGS_OF_EXPERTS, allReviews.size()));
-            updateExpertsWeightings(allReviews);
+            logger.info(String.format(RECALCULATING_WEIGHTINGS_OF_EXPERTS, reviews.size()));
+            updateExpertsWeightings(reviews);
         }
     }
 
