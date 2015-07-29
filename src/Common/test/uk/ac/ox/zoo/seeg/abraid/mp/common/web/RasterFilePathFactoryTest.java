7fe4af0c0dc7c645216b1261118e484e34439f60
Laurence Hudson <laurence.hudson@tessella.com>
2015-07-29 15:15:52 +0100

    Test new Raster Factory behaviour

--- src/Common/test/uk/ac/ox/zoo/seeg/abraid/mp/common/web/RasterFilePathFactoryTest.java
+++ src/Common/test/uk/ac/ox/zoo/seeg/abraid/mp/common/web/RasterFilePathFactoryTest.java
@@ -2,10 +2,13 @@ package uk.ac.ox.zoo.seeg.abraid.mp.common.web;
 
 import org.joda.time.DateTime;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import uk.ac.ox.zoo.seeg.abraid.mp.common.domain.ModelRun;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,55 +17,78 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Copyright (c) 2014 University of Oxford
  */
 public class RasterFilePathFactoryTest {
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder(); ///CHECKSTYLE:SUPPRESS VisibilityModifier
+
     private RasterFilePathFactory builder;
+    private String adminDirectory;
+    private String resultsDirectory;
 
     @Before
-    public void setUp() {
-        File root = new File(getCurrentDirectory());
-        builder = new RasterFilePathFactory(root);
+    public void setUp() throws IOException {
+        resultsDirectory = getResultsDirectory();
+        this.adminDirectory = getAdminDirectory();
+        builder = new RasterFilePathFactory(new File(resultsDirectory), new File(adminDirectory));
     }
 
     @Test
-    public void getFullMeanPredictionRasterFileReturnsCorrectFile() {
+    public void getFullMeanPredictionRasterFileReturnsCorrectFile() throws IOException {
         ModelRun modelRun = new ModelRun("testname", 87, "host", DateTime.now(), DateTime.now(), DateTime.now());
         File file = builder.getFullMeanPredictionRasterFile(modelRun);
         assertThat(file.getName()).isEqualTo("testname_mean_full.tif");
-        assertThat(file.getParent()).isEqualTo(getCurrentDirectory());
+        assertThat(file.getParent()).isEqualTo(resultsDirectory);
     }
 
     @Test
-    public void getMaskedMeanPredictionRasterFileReturnsCorrectFile() {
+    public void getMaskedMeanPredictionRasterFileReturnsCorrectFile() throws IOException {
         ModelRun modelRun = new ModelRun("testname", 87, "host", DateTime.now(), DateTime.now(), DateTime.now());
         File file = builder.getMaskedMeanPredictionRasterFile(modelRun);
         assertThat(file.getName()).isEqualTo("testname_mean.tif");
-        assertThat(file.getParent()).isEqualTo(getCurrentDirectory());
+        assertThat(file.getParent()).isEqualTo(resultsDirectory);
     }
 
     @Test
-    public void getFullPredictionUncertaintyRasterFileReturnsCorrectFile() {
+    public void getFullPredictionUncertaintyRasterFileReturnsCorrectFile() throws IOException {
         ModelRun modelRun = new ModelRun("testname", 87, "host", DateTime.now(), DateTime.now(), DateTime.now());
         File file = builder.getFullPredictionUncertaintyRasterFile(modelRun);
         assertThat(file.getName()).isEqualTo("testname_uncertainty_full.tif");
-        assertThat(file.getParent()).isEqualTo(getCurrentDirectory());
+        assertThat(file.getParent()).isEqualTo(resultsDirectory);
     }
 
     @Test
-    public void getMaskedPredictionUncertaintyRasterFileReturnsCorrectFile() {
+    public void getMaskedPredictionUncertaintyRasterFileReturnsCorrectFile() throws IOException {
         ModelRun modelRun = new ModelRun("testname", 87, "host", DateTime.now(), DateTime.now(), DateTime.now());
         File file = builder.getMaskedPredictionUncertaintyRasterFile(modelRun);
         assertThat(file.getName()).isEqualTo("testname_uncertainty.tif");
-        assertThat(file.getParent()).isEqualTo(getCurrentDirectory());
+        assertThat(file.getParent()).isEqualTo(resultsDirectory);
     }
 
     @Test
-    public void getExtentInputRasterFileReturnsCorrectFile() {
+    public void getExtentInputRasterFileReturnsCorrectFile() throws IOException {
         ModelRun modelRun = new ModelRun("testname", 87, "host", DateTime.now(), DateTime.now(), DateTime.now());
         File file = builder.getExtentInputRasterFile(modelRun);
         assertThat(file.getName()).isEqualTo("testname_extent.tif");
-        assertThat(file.getParent()).isEqualTo(getCurrentDirectory());
+        assertThat(file.getParent()).isEqualTo(resultsDirectory);
     }
 
-    private String getCurrentDirectory() {
-        return System.getProperty("user.dir");
+    @Test
+    public void getExtentAdminRasterFileReturnsCorrectFile() throws IOException {
+        File file = builder.getAdminRaster(0);
+        assertThat(file.getName()).isEqualTo("admin0qc.tif");
+        assertThat(file.getParent()).isEqualTo(adminDirectory);
+        file = builder.getAdminRaster(1);
+        assertThat(file.getName()).isEqualTo("admin1qc.tif");
+        assertThat(file.getParent()).isEqualTo(adminDirectory);
+        file = builder.getAdminRaster(2);
+        assertThat(file.getName()).isEqualTo("admin2qc.tif");
+        assertThat(file.getParent()).isEqualTo(adminDirectory);
+    }
+
+    private String getResultsDirectory() throws IOException {
+        return testFolder.newFolder().getAbsolutePath();
+    }
+
+    private String getAdminDirectory() throws IOException {
+        return testFolder.newFolder().getAbsolutePath();
     }
 }
