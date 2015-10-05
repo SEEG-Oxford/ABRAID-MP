527d9c5dac7af1a74b29bb5bfd4700390bacbb25
Laurence Hudson <laurence.hudson@tessella.com>
2015-10-05 18:21:47 +0100

    Give DataManager access to more memory

--- src/DataManager/src/uk/ac/ox/zoo/seeg/abraid/mp/datamanager/config/datamanager.sh
+++ src/DataManager/src/uk/ac/ox/zoo/seeg/abraid/mp/datamanager/config/datamanager.sh
@@ -8,4 +8,4 @@ echo Starting Data Manager...
 cd `dirname $0`
 
 # Run the Data Manager main class, passing in any arguments provided to this script
-java -Dlog4j.configuration=file:log4j.properties -cp "lib/*" uk.ac.ox.zoo.seeg.abraid.mp.datamanager.Main $*
+java -Xmx3g -Dlog4j.configuration=file:log4j.properties -cp "lib/*" uk.ac.ox.zoo.seeg.abraid.mp.datamanager.Main $*
