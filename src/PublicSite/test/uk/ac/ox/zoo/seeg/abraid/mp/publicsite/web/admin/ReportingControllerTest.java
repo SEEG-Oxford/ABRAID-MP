db46cb8f6e44b11279c58e8bdabe9527bfcbaede
Laurence Hudson <laurence.hudson@tessella.com>
2015-10-29 09:56:44 +0000

    Stylecop fix

--- src/PublicSite/test/uk/ac/ox/zoo/seeg/abraid/mp/publicsite/web/admin/ReportingControllerTest.java
+++ src/PublicSite/test/uk/ac/ox/zoo/seeg/abraid/mp/publicsite/web/admin/ReportingControllerTest.java
@@ -59,22 +59,22 @@ public class ReportingControllerTest {
 
         assertThat(months).isEqualTo(Arrays.asList("2015-02", "2015-03", "2015-04", "Total"));
         assertThat(qualifiers).isEqualTo(Arrays.asList("Dengue", "Cholera", "Other", "Total"));
-        assertThat(data.get("2015-02").get("Dengue") ).isEqualTo(new HealthMapReportEntry("2015-02", "Dengue",   0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
+        assertThat(data.get("2015-02").get("Dengue")) .isEqualTo(new HealthMapReportEntry("2015-02", "Dengue",   0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
         assertThat(data.get("2015-02").get("Cholera")).isEqualTo(new HealthMapReportEntry("2015-02", "Cholera",  0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
-        assertThat(data.get("2015-02").get("Other")  ).isEqualTo(new HealthMapReportEntry("2015-02", "Other",    0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
-        assertThat(data.get("2015-02").get("Total")  ).isEqualTo(new HealthMapReportEntry("2015-02", "Total",    0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
-        assertThat(data.get("2015-03").get("Dengue") ).isEqualTo(new HealthMapReportEntry("2015-03", "Dengue",   1L,  2L,  3L,  4L,  5L,  6L,  7L,  8L));
+        assertThat(data.get("2015-02").get("Other"))  .isEqualTo(new HealthMapReportEntry("2015-02", "Other",    0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
+        assertThat(data.get("2015-02").get("Total"))  .isEqualTo(new HealthMapReportEntry("2015-02", "Total",    0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
+        assertThat(data.get("2015-03").get("Dengue")) .isEqualTo(new HealthMapReportEntry("2015-03", "Dengue",   1L,  2L,  3L,  4L,  5L,  6L,  7L,  8L));
         assertThat(data.get("2015-03").get("Cholera")).isEqualTo(new HealthMapReportEntry("2015-03", "Cholera",  8L,  7L,  6L,  5L,  4L,  3L,  2L,  1L));
-        assertThat(data.get("2015-03").get("Other")  ).isEqualTo(new HealthMapReportEntry("2015-03", "Other",    0L,  3L,  3L,  0L,  0L,  3L,  3L,  0L));
-        assertThat(data.get("2015-03").get("Total")  ).isEqualTo(new HealthMapReportEntry("2015-03", "Total",    9L, 12L, 12L,  9L,  9L, 12L, 12L,  9L));
-        assertThat(data.get("2015-04").get("Dengue") ).isEqualTo(new HealthMapReportEntry("2015-04", "Dengue",   0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
+        assertThat(data.get("2015-03").get("Other"))  .isEqualTo(new HealthMapReportEntry("2015-03", "Other",    0L,  3L,  3L,  0L,  0L,  3L,  3L,  0L));
+        assertThat(data.get("2015-03").get("Total"))  .isEqualTo(new HealthMapReportEntry("2015-03", "Total",    9L, 12L, 12L,  9L,  9L, 12L, 12L,  9L));
+        assertThat(data.get("2015-04").get("Dengue")) .isEqualTo(new HealthMapReportEntry("2015-04", "Dengue",   0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
         assertThat(data.get("2015-04").get("Cholera")).isEqualTo(new HealthMapReportEntry("2015-04", "Cholera",  1L,  0L,  1L,  0L,  1L,  1L,  0L,  1L));
-        assertThat(data.get("2015-04").get("Other")  ).isEqualTo(new HealthMapReportEntry("2015-04", "Other",    1L,  1L,  1L,  1L,  1L,  1L,  1L,  1L));
-        assertThat(data.get("2015-04").get("Total")  ).isEqualTo(new HealthMapReportEntry("2015-04", "Total",    2L,  1L,  2L,  1L,  2L,  2L,  1L,  2L));
-        assertThat(data.get("Total")  .get("Dengue") ).isEqualTo(new HealthMapReportEntry("Total",   "Dengue",   1L,  2L,  3L,  4L,  5L,  6L,  7L,  8L));
+        assertThat(data.get("2015-04").get("Other"))  .isEqualTo(new HealthMapReportEntry("2015-04", "Other",    1L,  1L,  1L,  1L,  1L,  1L,  1L,  1L));
+        assertThat(data.get("2015-04").get("Total"))  .isEqualTo(new HealthMapReportEntry("2015-04", "Total",    2L,  1L,  2L,  1L,  2L,  2L,  1L,  2L));
+        assertThat(data.get("Total")  .get("Dengue")) .isEqualTo(new HealthMapReportEntry("Total",   "Dengue",   1L,  2L,  3L,  4L,  5L,  6L,  7L,  8L));
         assertThat(data.get("Total")  .get("Cholera")).isEqualTo(new HealthMapReportEntry("Total",   "Cholera",  9L,  7L,  7L,  5L,  5L,  4L,  2L,  2L));
-        assertThat(data.get("Total")  .get("Other")  ).isEqualTo(new HealthMapReportEntry("Total",   "Other",    1L,  4L,  4L,  1L,  1L,  4L,  4L,  1L));
-        assertThat(data.get("Total")  .get("Total")  ).isEqualTo(new HealthMapReportEntry("Total",   "Total",   11L, 13L, 14L, 10L, 11L, 14L, 13L, 11L));
+        assertThat(data.get("Total")  .get("Other"))  .isEqualTo(new HealthMapReportEntry("Total",   "Other",    1L,  4L,  4L,  1L,  1L,  4L,  4L,  1L));
+        assertThat(data.get("Total")  .get("Total"))  .isEqualTo(new HealthMapReportEntry("Total",   "Total",   11L, 13L, 14L, 10L, 11L, 14L, 13L, 11L));
     }
 
     @Test
@@ -114,20 +114,20 @@ public class ReportingControllerTest {
 
         assertThat(months).isEqualTo(Arrays.asList("2015-02", "2015-03", "2015-04", "Total"));
         assertThat(qualifiers).isEqualTo(Arrays.asList("UK", "USA", "Other", "Total"));
-        assertThat(data.get("2015-02").get("UK")   ).isEqualTo(new HealthMapReportEntry("2015-02", "UK",     0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
-        assertThat(data.get("2015-02").get("USA")  ).isEqualTo(new HealthMapReportEntry("2015-02", "USA",    0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
+        assertThat(data.get("2015-02").get("UK"))   .isEqualTo(new HealthMapReportEntry("2015-02", "UK",     0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
+        assertThat(data.get("2015-02").get("USA"))  .isEqualTo(new HealthMapReportEntry("2015-02", "USA",    0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
         assertThat(data.get("2015-02").get("Other")).isEqualTo(new HealthMapReportEntry("2015-02", "Other",  0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
         assertThat(data.get("2015-02").get("Total")).isEqualTo(new HealthMapReportEntry("2015-02", "Total",  0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
-        assertThat(data.get("2015-03").get("UK")   ).isEqualTo(new HealthMapReportEntry("2015-03", "UK",     1L,  2L,  3L,  4L,  5L,  6L,  7L,  8L));
-        assertThat(data.get("2015-03").get("USA")  ).isEqualTo(new HealthMapReportEntry("2015-03", "USA",    8L,  7L,  6L,  5L,  4L,  3L,  2L,  1L));
+        assertThat(data.get("2015-03").get("UK"))   .isEqualTo(new HealthMapReportEntry("2015-03", "UK",     1L,  2L,  3L,  4L,  5L,  6L,  7L,  8L));
+        assertThat(data.get("2015-03").get("USA"))  .isEqualTo(new HealthMapReportEntry("2015-03", "USA",    8L,  7L,  6L,  5L,  4L,  3L,  2L,  1L));
         assertThat(data.get("2015-03").get("Other")).isEqualTo(new HealthMapReportEntry("2015-03", "Other",  0L,  3L,  3L,  0L,  0L,  3L,  3L,  0L));
         assertThat(data.get("2015-03").get("Total")).isEqualTo(new HealthMapReportEntry("2015-03", "Total",  9L, 12L, 12L,  9L,  9L, 12L, 12L,  9L));
-        assertThat(data.get("2015-04").get("UK")   ).isEqualTo(new HealthMapReportEntry("2015-04", "UK",     0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
-        assertThat(data.get("2015-04").get("USA")  ).isEqualTo(new HealthMapReportEntry("2015-04", "USA",    1L,  0L,  1L,  0L,  1L,  1L,  0L,  1L));
+        assertThat(data.get("2015-04").get("UK"))   .isEqualTo(new HealthMapReportEntry("2015-04", "UK",     0L,  0L,  0L,  0L,  0L,  0L,  0L,  0L));
+        assertThat(data.get("2015-04").get("USA"))  .isEqualTo(new HealthMapReportEntry("2015-04", "USA",    1L,  0L,  1L,  0L,  1L,  1L,  0L,  1L));
         assertThat(data.get("2015-04").get("Other")).isEqualTo(new HealthMapReportEntry("2015-04", "Other",  1L,  1L,  1L,  1L,  1L,  1L,  1L,  1L));
         assertThat(data.get("2015-04").get("Total")).isEqualTo(new HealthMapReportEntry("2015-04", "Total",  2L,  1L,  2L,  1L,  2L,  2L,  1L,  2L));
-        assertThat(data.get("Total")  .get("UK")   ).isEqualTo(new HealthMapReportEntry("Total",   "UK",     1L,  2L,  3L,  4L,  5L,  6L,  7L,  8L));
-        assertThat(data.get("Total")  .get("USA")  ).isEqualTo(new HealthMapReportEntry("Total",   "USA",    9L,  7L,  7L,  5L,  5L,  4L,  2L,  2L));
+        assertThat(data.get("Total")  .get("UK"))   .isEqualTo(new HealthMapReportEntry("Total",   "UK",     1L,  2L,  3L,  4L,  5L,  6L,  7L,  8L));
+        assertThat(data.get("Total")  .get("USA"))  .isEqualTo(new HealthMapReportEntry("Total",   "USA",    9L,  7L,  7L,  5L,  5L,  4L,  2L,  2L));
         assertThat(data.get("Total")  .get("Other")).isEqualTo(new HealthMapReportEntry("Total",   "Other",  1L,  4L,  4L,  1L,  1L,  4L,  4L,  1L));
         assertThat(data.get("Total")  .get("Total")).isEqualTo(new HealthMapReportEntry("Total",   "Total",  11L, 13L, 14L, 10L, 11L, 14L, 13L, 11L));
     }
