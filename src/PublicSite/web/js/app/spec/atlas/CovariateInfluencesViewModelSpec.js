ac08a3ad73818a0d88c4c388843a574c9463c233
Laurence Hudson <laurence.hudson@tessella.com>
2015-11-30 13:30:30 +0000

    Test changes

--- src/PublicSite/web/js/app/spec/atlas/CovariateInfluencesViewModelSpec.js
+++ src/PublicSite/web/js/app/spec/atlas/CovariateInfluencesViewModelSpec.js
@@ -100,7 +100,7 @@ define([
                 vm.covariateInfluences([
                     { meanInfluence: 101, effectCurve: [] }
                 ]);
-                expect(vm.covariateInfluencesToPlot()[0]).toEqual({ meanInfluence: 101, effectCurve: [] });
+                expect(vm.covariateInfluencesToPlot()[0].covariate).toEqual({ meanInfluence: 101, effectCurve: [] });
                 expect(vm.covariateInfluencesToPlot().length).toEqual(1);
 
                 vm.covariateInfluences([
@@ -108,10 +108,52 @@ define([
                     { meanInfluence: 10, effectCurve: [] },
                     { meanInfluence: 50, effectCurve: [] }
                 ]);
-                expect(vm.covariateInfluencesToPlot()[0]).toEqual({ meanInfluence: 100, effectCurve: [] });
-                expect(vm.covariateInfluencesToPlot()[1]).toEqual({ meanInfluence: 50, effectCurve: [] });
+                expect(vm.covariateInfluencesToPlot()[0].covariate).toEqual({ meanInfluence: 100, effectCurve: [] });
+                expect(vm.covariateInfluencesToPlot()[1].covariate).toEqual({ meanInfluence: 50, effectCurve: [] });
                 expect(vm.covariateInfluencesToPlot().length).toEqual(2);
             });
+
+            it("is adds a max value to each entry, which is the max upper quantile in the filtered covariates",
+                function () {
+                    vm.covariateInfluences([
+                        { meanInfluence: 101, effectCurve:
+                            [ { upperQuantile: 1}, { upperQuantile: 5}, { upperQuantile: 2 } ] }
+                    ]);
+                    expect(vm.covariateInfluencesToPlot()[0].max).toEqual(5);
+
+                    vm.covariateInfluences([
+                        { meanInfluence: 100, effectCurve:
+                            [ { upperQuantile: 3 }, { upperQuantile: 2 }, { upperQuantile: 1 } ] },
+                        { meanInfluence: 10, effectCurve:
+                            [ { upperQuantile: 10 }, { upperQuantile: -10 }, { upperQuantile: -2 } ] },
+                        { meanInfluence: 50, effectCurve:
+                            [ { upperQuantile: -2 }, { upperQuantile: -5 }, { upperQuantile: 3 } ] }
+                    ]);
+                    expect(vm.covariateInfluencesToPlot()[0].max).toEqual(3);
+                    expect(vm.covariateInfluencesToPlot()[1].max).toEqual(3);
+                }
+            );
+
+            it("is adds a min value to each entry, which is the min lower quantile in the filtered covariates",
+                function () {
+                    vm.covariateInfluences([
+                        { meanInfluence: 101, effectCurve:
+                            [ { lowerQuantile: 1}, { lowerQuantile: 5}, { lowerQuantile: 2 } ] }
+                    ]);
+                    expect(vm.covariateInfluencesToPlot()[0].min).toEqual(1);
+
+                    vm.covariateInfluences([
+                        { meanInfluence: 100, effectCurve:
+                            [ { lowerQuantile: 3 }, { lowerQuantile: 2 }, { lowerQuantile: 1 } ] },
+                        { meanInfluence: 10, effectCurve:
+                            [ { lowerQuantile: 10 }, { lowerQuantile: -10 }, { lowerQuantile: -2 } ] },
+                        { meanInfluence: 50, effectCurve:
+                            [ { lowerQuantile: -2 }, { lowerQuantile: -5 }, { lowerQuantile: 3 } ] }
+                    ]);
+                    expect(vm.covariateInfluencesToPlot()[0].min).toEqual(-5);
+                    expect(vm.covariateInfluencesToPlot()[1].min).toEqual(-5);
+                }
+            );
         });
 
         describe("holds the active curve field which", function () {
@@ -123,54 +165,5 @@ define([
                 expect(vm.activeCurve()).toBeUndefined();
             });
         });
-
-        describe("holds the min effect curve value field which", function () {
-            it("is observable", function () {
-                expect(vm.minEffectCurveValue).toBeObservable();
-            });
-
-            it("is a min lower quantile in the effect curves of the covariates to plot", function () {
-                vm.covariateInfluences([
-                    { meanInfluence: 101, effectCurve:
-                        [ { lowerQuantile: 1}, { lowerQuantile: 5}, { lowerQuantile: 2 } ] }
-                ]);
-                expect(vm.minEffectCurveValue()).toEqual(1);
-
-                vm.covariateInfluences([
-                    { meanInfluence: 100, effectCurve:
-                        [ { lowerQuantile: 3 }, { lowerQuantile: 2 }, { lowerQuantile: 1 } ] },
-                    { meanInfluence: 10, effectCurve:
-                        [ { lowerQuantile: 10 }, { lowerQuantile: -10 }, { lowerQuantile: -2 } ] },
-                    { meanInfluence: 50, effectCurve:
-                        [ { lowerQuantile: -2 }, { lowerQuantile: -5 }, { lowerQuantile: 3 } ] }
-                ]);
-                expect(vm.minEffectCurveValue()).toEqual(-5);
-            });
-        });
-
-        describe("holds the max effect curve value field which", function () {
-            it("is observable", function () {
-                expect(vm.maxEffectCurveValue).toBeObservable();
-            });
-
-            it("is a max upper quantile in the effect curves of the covariates to plot", function () {
-                vm.covariateInfluences([
-                    { meanInfluence: 101, effectCurve:
-                        [ { upperQuantile: 1}, { upperQuantile: 5}, { upperQuantile: 2 } ] }
-                ]);
-                expect(vm.maxEffectCurveValue()).toEqual(5);
-
-                vm.covariateInfluences([
-                    { meanInfluence: 100, effectCurve:
-                        [ { upperQuantile: 3 }, { upperQuantile: 2 }, { upperQuantile: 1 } ] },
-                    { meanInfluence: 10, effectCurve:
-                        [ { upperQuantile: 10 }, { upperQuantile: -10 }, { upperQuantile: -2 } ] },
-                    { meanInfluence: 50, effectCurve:
-                        [ { upperQuantile: -2 }, { upperQuantile: -5 }, { upperQuantile: 3 } ] }
-                ]);
-                expect(vm.maxEffectCurveValue()).toEqual(3);
-            });
-        });
-
     });
 });
